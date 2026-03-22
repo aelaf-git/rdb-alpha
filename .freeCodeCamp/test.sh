dd35e4312e17f28d02244a2c5414e01a973b2b0b
Aelaf Eskindir Abebe <aelafeskindir124@gmail.com>
2026-03-22 04:40:09 -0400

    Save progress: 1190.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -2,6 +2,9 @@
 # Program to tell a persons fortune
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-
-N=$(( RANDOM%1 ))
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+}
+N=$(( RANDOM%6 ))
 echo ${RESPONSES[$N]}
+GET_FORTUNE
