8b0e270f0cdb133ea329960393ad86191fab027d
Aelaf Eskindir Abebe <aelafeskindir124@gmail.com>
2026-03-22 04:56:43 -0400

    Save progress: 1260.1

--- fortune.sh
+++ fortune.sh
@@ -6,6 +6,10 @@ GET_FORTUNE() {
   echo Ask a yes or no question:
   read QUESTION
 }
+until [[ $QUESTION == test? ]]
+do
+  GET_FORTUNE
+done
 N=$(( RANDOM%6 ))
-GET_FORTUNE
+
 echo ${RESPONSES[$N]}
