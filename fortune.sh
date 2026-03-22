0cb4957bef2f0f30e3b7bd194260bd303d09c12a
Aelaf Eskindir Abebe <aelafeskindir124@gmail.com>
2026-03-22 04:47:24 -0400

    Progress

---
+++ fortune.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program to tell a persons fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+  read QUESTION
+}
+N=$(( RANDOM%6 ))
+GET_FORTUNE
+echo ${RESPONSES[$N]}
